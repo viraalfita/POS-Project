93b466e818c7186ee38e84385f92972c13c304fa
Vira Alfita <viraalfita1813@gmail.com>
2025-05-12 02:36:13 +0700

    praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -102,6 +102,7 @@ Route::middleware(['auth'])->group(function () {
         Route::delete('/{id}', [BarangController::class, 'destroy']);
         Route::get('/import', [BarangController::class, 'import']);
         Route::post('/import_ajax', [BarangController::class, 'import_ajax']);
+        Route::get('/export_excel', [BarangController::class, 'export_excel']);
     });
 
     Route::group([
